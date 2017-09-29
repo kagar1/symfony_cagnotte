5034aa9e7a3c9ad06ecb5c9b44cfdf4f28d4cdfb
Karim <ngami.karim@yahoo.fr>
2017-09-29 11:47:00 +0100

    ajout de Fosuser dans appkernel

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,19 +17,23 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new AppBundle\AppBundle(),
             new Core\CagnotteBundle\CoreCagnotteBundle(),
+            new FOS\UserBundle\FOSUserBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
             $bundles[] = new Symfony\Bundle\DebugBundle\DebugBundle();
             $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
             $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
+            
 
             if ('dev' === $this->getEnvironment()) {
                 $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
                 $bundles[] = new Symfony\Bundle\WebServerBundle\WebServerBundle();
             }
         }
-
+        
+        
+        
         return $bundles;
     }
 
